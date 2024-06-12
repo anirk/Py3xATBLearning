dac3088589bdcf0e8c174342db61e3888621c2e9
adiirk <43725757+adiirk@users.noreply.github.com>
2024-06-12 18:55:27 +0530

    practice

---
+++ Ex_05062024/Lab008.py
@@ -0,0 +1,12 @@
+name = "Anitha"
+age= 20
+pi= 3.144
+isFemale = True
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(isFemale))
+
+complex_number= 2+3j
+print((complex_number.real))
+print(complex_number.imag)
