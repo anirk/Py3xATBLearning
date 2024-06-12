5ba935f6a7c74b9efd3933982aa5b408adbb7bb4
adiirk <43725757+adiirk@users.noreply.github.com>
2024-06-12 19:32:51 +0530

    practice

--- Ex_05062024/Lab010.py
+++ Ex_05062024/Lab010.py
@@ -3,3 +3,10 @@ _name= "Anitha"
 first_name = "Anitha"
 long_var_bigger_name = "Anitha"
 print(long_var_bigger_name)
+
+long_var_bigger_name1="""
+ddddddddddd
+dddddddddd
+ddddddddd
+"""
+print(long_var_bigger_name1)
