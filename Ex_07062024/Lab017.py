b9f2251ed2684413fe2e253d93ce3d4041804c95
adiirk <43725757+adiirk@users.noreply.github.com>
2024-06-13 20:08:46 +0530

    practice

---
+++ Ex_07062024/Lab017.py
@@ -0,0 +1,19 @@
+name='Harry'
+print(name)
+name = "Harry"
+print(name)
+name= """Harry is good person.
+He walks a lot"""
+print(name)
+
+
+dir = r'C:\somedir\newwdir'
+print(dir)
+
+first_name= "Harry"
+last_name= "Potter"
+print(first_name+"  " +last_name)
+print(first_name, last_name)
+print(f'Your name is {first_name} {last_name}')
+
+
