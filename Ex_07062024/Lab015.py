2ab533a9d76d13db950ee2e3eb463840496c7aa5
adiirk <43725757+adiirk@users.noreply.github.com>
2024-06-13 19:52:25 +0530

    practice

---
+++ Ex_07062024/Lab015.py
@@ -0,0 +1,9 @@
+num1 =int(input("enter first number: "))
+num2 =int(input("enter second number: "))
+result = num1+num2
+print(result)
+
+num1 =input("enter first number: ")
+num2 =input("enter second number: ")
+result = int(num1)+int(num2)
+print(result)
---
+++ Ex_07062024/Lab016.py
@@ -0,0 +1,7 @@
+pi= 3.14
+print(type(pi))
+
+i = 12
+i2 = 10
+result = i/i2
+print(type(result))
