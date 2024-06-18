583ced3d9f33d10c2eb8a56ea16779c699e09766
adiirk <43725757+adiirk@users.noreply.github.com>
2024-06-18 19:22:12 +0530

    practice

---
+++ Ex_10062024/Lab025.py
@@ -0,0 +1,8 @@
+name="Anitha"
+print(type(name))
+pi=3.14
+print(type(pi))
+my_list=[1,2,3,4,5]
+print(type(my_list))
+my_set={1,2,3,4,5}
+print(type(my_set))
---
+++ Ex_10062024/Lab026.py
@@ -0,0 +1,20 @@
+v1=(1==True)
+v2=(0==False)
+print(v1)
+
+age=65
+num=-1
+r=age+num
+print(r)
+
+is_good=True
+print((not is_good))
+
+a=5
+b=6
+
+print( a is b)
+
+my_list=[1,2,3,4]
+my_list2=[1,2,3,4]
+print(my_list is my_list2)
