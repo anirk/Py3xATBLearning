2edd292e0401f2f0a7f4c8fcd7047fd82c672452
Anitha <anithark083@gmail.com>
2024-06-25 20:15:18 +0530

    practice

---
+++ Ex_10062024/Lab030.py
@@ -0,0 +1,3 @@
+s1_marks=10
+s2_marks=20
+print("anitha is winner" if s1_marks>s2_marks else "s2 is winner" )
---
+++ Ex_10062024/Lab031.py
@@ -0,0 +1,9 @@
+import math
+radius=input("Enter the radius\n")
+print(math.pi)
+radius=float(radius)
+area= math.pi*(radius**2)
+print(area)
+
+area1=3.14*(float(input("Enter the radius\n"))**2)
+print(area1)
